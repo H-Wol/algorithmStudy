74275999687a58ed6500e7c90dcd1c24c05469bb
H_wall <hunyoon435@gmail.com>
2023-04-02 18:54:35 +0900

    feat: 15652번 문제 해결

---
+++ beakjoon/class4/15652_N과 M(4).py
@@ -0,0 +1,17 @@
+N, M = map(int, input().split(" "))
+
+answer = []
+
+
+def DFS(start):
+    if len(answer) == M:
+        print(" ".join(map(str, answer)))
+        return
+    for i in range(start, N + 1):
+        # if i not in answer:
+        answer.append(i)
+        DFS(i)
+        answer.pop()
+
+
+DFS(1)
