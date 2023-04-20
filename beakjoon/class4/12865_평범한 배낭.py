2384283aa88081765bd79564ff74f00ec2c7b7e8
yoon <hunyoon435@gmail.com>
2023-04-20 18:16:29 +0900

    feat: 12865번 문제 해결

---
+++ beakjoon/class4/12865_평범한 배낭.py
@@ -0,0 +1,24 @@
+from sys import stdin
+
+input = stdin.readline
+
+N, K = map(int, input().split(" "))
+
+arr = [[0] * (K + 1) for _ in range(N + 1)]
+
+
+stuff = [[0, 0]]
+
+for _ in range(N):
+    stuff.append(list(map(int, input().split(" "))))
+
+for i in range(1, N + 1):
+    for j in range(1, K + 1):
+        w = stuff[i][0]
+        val = stuff[i][1]
+        if w > j:
+            arr[i][j] = arr[i - 1][j]
+        else:
+            arr[i][j] = max(val + arr[i - 1][j - w], arr[i - 1][j])
+print(arr[N][K])
+print(arr)
