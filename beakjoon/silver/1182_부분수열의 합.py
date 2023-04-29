d635a74994fe667b3553954432ca1d2f702d6dc5
H_wall <hunyoon435@gmail.com>
2023-04-29 14:46:08 +0900

    feat: 1182번 문제 해결

---
+++ beakjoon/silver/1182_부분수열의 합.py
@@ -0,0 +1,15 @@
+from itertools import combinations
+from sys import stdin
+
+input = stdin.readline
+
+N, S = map(int, input().split())
+groups = list(map(int, input().split()))
+count = 0
+for i in range(1, N + 1):
+    sub = list(map(sum, combinations(groups, i)))
+    count += sub.count(S)
+print(count)
+# for j in sub:
+#     print(sum(list(j)))
+# print("\n")
