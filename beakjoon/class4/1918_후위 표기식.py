b886ae2a9cc15aacddbfe25149c7ff5dbb6b8399
yoon <hunyoon435@gmail.com>
2023-05-06 22:36:27 +0900

    feat: 1918번 문제 해결

---
+++ beakjoon/class4/1918_후위 표기식.py
@@ -0,0 +1,40 @@
+from sys import stdin
+from collections import deque
+
+sen = stdin.readline().rstrip()
+
+answers = []
+exps = deque([])
+
+
+exp_score = {"+": 0, "-": 0, "*": 1, "/": 1, "(": 2, ")": 2}
+
+for s in sen:
+    if s not in exp_score:
+        answers.append(s)
+        continue
+    if len(exps) == 0 or s == "(":
+        exps.append(s)
+        continue
+    if s == ")":
+        while exps:
+            if exps[-1] == "(":
+                exps.pop()
+                break
+            else:
+                answers.append(exps.pop())
+        continue
+    if s == "+" or s == "-":
+        while exps and exps[-1] != "(":
+            answers.append(exps.pop())
+        exps.append(s)
+        continue
+    if s == "*" or s == "/":
+        while exps and exp_score[exps[-1]] == 1:
+            answers.append(exps.pop())
+        exps.append(s)
+        continue
+
+while exps:
+    answers.append(exps.pop())
+print("".join(answers))
