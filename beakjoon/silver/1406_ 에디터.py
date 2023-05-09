3d5b283108d345fc25606fdfc2c2a52e60f5cae7
yoon <hunyoon435@gmail.com>
2023-05-09 20:36:19 +0900

    feat: 1406번 문제 해결

---
+++ beakjoon/silver/1406_ 에디터.py
@@ -0,0 +1,94 @@
+class Node:
+    def __init__(self, data, next=None, front=None):
+        self.data = data
+        self.next = next
+        self.front = front
+
+
+from sys import stdin
+
+input = stdin.readline
+
+
+datas = list(input().rstrip())
+nodes = [Node(datas[0])]
+for i in range(1, len(datas)):
+    node = Node(datas[i], None, nodes[i - 1])
+    nodes.append(node)
+    nodes[i - 1].next = node
+
+node = Node(None, None, nodes[-1])
+nodes.append(node)
+nodes[-2].next = node
+
+
+cur = nodes[-1]
+HEAD = nodes[0]
+
+M = int(input())
+
+
+def L(cur: Node):
+    if cur.front == None:
+        return cur
+    return cur.front
+
+
+def D(cur: Node):
+    if cur.next == None:
+        return cur
+    return cur.next
+
+
+def B(cur: Node):
+    global HEAD
+    if cur.front == None:
+        return cur
+    if cur.front.front == None:
+        cur.front = None
+        HEAD = cur
+        return cur
+    if cur.front.front == HEAD:
+        HEAD = cur
+    cur.front.front.next = cur
+    cur.front = cur.front.front
+    return cur
+
+
+def P(cur: Node, s: str):
+    global HEAD
+    node = Node(s, cur, cur.front)
+    nodes.append(node)
+    if cur.front == None:
+        cur.front = node
+        HEAD = node
+        return cur
+    cur.front.next = node
+    cur.front = node
+    return cur
+
+
+func = {
+    "L": L,
+    "D": D,
+    "B": B,
+    "P": P,
+}
+
+for _ in range(M):
+    inputs = input().rstrip()
+    if inputs.startswith("P"):
+        cur = P(cur, inputs.split()[-1])
+    else:
+        cur = func[inputs](cur)
+
+if HEAD == None or HEAD.data == None:
+    print("")
+    exit()
+answer = []
+while True:
+    answer.append(HEAD.data)
+    HEAD = HEAD.next
+    if HEAD == None or HEAD.data == None:
+        break
+print("".join(answer))
