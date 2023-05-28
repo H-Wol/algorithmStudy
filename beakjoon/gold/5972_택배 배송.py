f9be7367fb203d1302e6503c99afa347b37755b7
yoon <hunyoon435@gmail.com>
2023-05-28 18:48:34 +0900

    feat: 백준 5972번 문제 해결

---
+++ beakjoon/gold/5972_택배 배송.py
@@ -0,0 +1,35 @@
+from sys import stdin
+import heapq
+
+
+input = stdin.readline
+
+N, M = map(int, input().split())
+
+nodes = [[] for _ in range(N)]
+
+
+for _ in range(M):
+    start, end, val = map(int, input().split())
+    nodes[start - 1].append((end - 1, val))
+    nodes[end - 1].append((start - 1, val))
+
+distance = [float("inf") for _ in range(N)]
+
+
+def dijkstra(start, nodes, distance):
+    heap = []
+    distance[start - 1] = 0
+    heapq.heappush(heap, (distance[start - 1], start - 1))
+    while heap:
+        d, now = heapq.heappop(heap)
+        if distance[now] < d:
+            continue
+        for next, val in nodes[now]:
+            if distance[next] > d + val:
+                distance[next] = d + val
+                heapq.heappush(heap, (distance[next], next))
+    return distance
+
+
+print(dijkstra(1, nodes, distance)[-1])
