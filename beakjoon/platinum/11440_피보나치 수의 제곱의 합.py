f73341ace9b6cf4c7b4683bbc50cafa0f2b23afd
yoon <hunyoon435@gmail.com>
2023-06-16 19:01:45 +0900

    feat: 백준 11440번 문제 해결

---
+++ beakjoon/platinum/11440_피보나치 수의 제곱의 합.py
@@ -0,0 +1,24 @@
+def matrix_multi(A, B):
+    temp = [[0] * (len(A)) for _ in range(len(B[0]))]
+    for i in range(len(A)):
+        for j in range(len(A[0])):
+            for k in range(len(B[0])):
+                temp[i][k] += (A[i][j] * B[j][k]) % 1_000_000_007
+    return temp
+
+
+def matrix_pow(A, n):
+    if n == 1:
+        return A
+    if n % 2 == 0:
+        temp = matrix_pow(A, n // 2)
+        return matrix_multi(temp, temp)
+    else:
+        temp = matrix_pow(A, n - 1)
+        return matrix_multi(temp, A)
+
+
+from sys import stdin
+
+res = matrix_pow([[1, 1], [1, 0]], int(stdin.readline()))
+print(res[0][1] * res[0][0] % 1_000_000_007)
