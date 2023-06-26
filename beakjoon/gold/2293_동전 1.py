b62ab83a709b44f8c0226cf37b94859067fe8ba5
yoon <hunyoon435@gmail.com>
2023-06-26 20:33:17 +0900

    feat: 백준 2293번 문제 해결

---
+++ beakjoon/gold/2293_동전 1.py
@@ -0,0 +1,16 @@
+from sys import stdin
+
+input = stdin.readline
+
+n, k = map(int, input().split())
+coins = [int(input()) for _ in range(n)]
+
+dp = [0 for _ in range(k + 1)]
+dp[0] = 1
+
+for coin in coins:
+    for i in range(coin, k + 1):
+        if i >= coin:
+            dp[i] += dp[i - coin]
+
+print(dp[k])
