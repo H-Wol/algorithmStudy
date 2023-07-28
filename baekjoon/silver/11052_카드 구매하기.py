eea404447ff250f36ee635c42130e2155b876ef4
H_wall <hunyoon435@gmail.com>
2023-07-28 17:28:21 +0900

    feat: 백준 11052번 문제 해결

---
+++ baekjoon/silver/11052_카드 구매하기.py
@@ -0,0 +1,14 @@
+from sys import stdin
+
+input = stdin.readline
+
+n = int(input())
+prices = [0] + list(map(int, input().rsplit()))
+
+dp = [0] * (n + 1)
+
+for i in range(1, n + 1):
+    for j in range(1, i + 1):
+        dp[i] = max(dp[i], dp[i - j] + prices[j])
+
+print(dp[n])
