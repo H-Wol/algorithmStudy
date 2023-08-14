b978894c10874be3fd032f47cb14aca0742fffd7
H_wall <hunyoon435@gmail.com>
2023-08-14 22:51:39 +0900

    feat: 백준 11057번 문제 해결

---
+++ baekjoon/silver/11057_오르막 수.py
@@ -0,0 +1,18 @@
+from sys import stdin
+
+input = stdin.readline
+
+n = int(input())
+
+dp = [[0] * 10 for _ in range(n + 1)]
+
+for i in range(10):
+    dp[1][i] = 1
+
+for i in range(2, n + 1):
+    for j in range(10):
+        for k in range(j + 1):
+            dp[i][j] += dp[i - 1][k]
+
+total_count = sum(dp[n]) % 10007
+print(total_count)
