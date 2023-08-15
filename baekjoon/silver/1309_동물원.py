ff80b7cf7ac7dc98518bbc74d3a9b623f506af9b
H_wall <hunyoon435@gmail.com>
2023-08-15 17:41:44 +0900

    feat: 백준 1309번 문제 해결

---
+++ baekjoon/silver/1309_동물원.py
@@ -0,0 +1,13 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+dp = [0] * (n + 2)
+dp[0], dp[1], dp[2] = 1, 3, 7
+
+for i in range(3, n + 1):
+    dp[i] = (2 * dp[i - 1] + dp[i - 2]) % 9901
+
+print(dp[n])
