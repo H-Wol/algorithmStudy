af7468d4dcd0dedec068a546bc552c0f1093814f
H_wall <hunyoon435@gmail.com>
2023-09-16 23:24:44 +0900

    feat: 백준 17175번 문제 해결

---
+++ baekjoon/silver/17175_피보나치는 지겨웡~.py
@@ -0,0 +1,13 @@
+from sys import stdin
+
+input = stdin.readline
+
+n = int(input())
+
+if n < 2:
+    print(1)
+else:
+    a, b = 1, 1
+    for i in range(n - 1):
+        a, b = a + b + 1, a
+    print(a % 1000000007)
