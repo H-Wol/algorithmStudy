6ba50ba09b846642aaeea9de3e17749fada7a4e6
H_Wall <hunyoon435@gmail.com>
2023-11-21 23:29:44 +0900

    feat: 백준 2688번 문제 해결

---
+++ baekjoon/silver/2688_줄어들지 않아.py
@@ -0,0 +1,21 @@
+from sys import stdin
+
+input = stdin.readline
+
+
+t = int(input())
+
+
+for _ in range(t):
+    n = int(input())
+    dp = [[0] * 10 for _ in range(n + 1)]
+
+    for i in range(10):
+        dp[1][i] = 1
+
+    for i in range(2, n + 1):
+        for j in range(10):
+            dp[i][j] = sum(dp[i - 1][:j + 1])
+
+    result = sum(dp[n])
+    print(result)
