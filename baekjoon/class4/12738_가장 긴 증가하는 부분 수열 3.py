3e89f2a8f51518c109add54e2e2dd8d5451a5795
H_Wall <hunyoon435@gmail.com>
2023-12-24 22:35:52 +0900

    feat: 백준 12738번 문제 해결

---
+++ baekjoon/class4/12738_가장 긴 증가하는 부분 수열 3.py
@@ -0,0 +1,35 @@
+from sys import stdin
+
+input = stdin.readline
+
+
+def binarySearch(min, max, target, arr):
+    mid = (min + max) // 2
+    if min > max:
+        return -1
+    if arr[mid] < target <= arr[mid + 1]:
+        return mid + 1
+    if arr[mid] < target:
+        return binarySearch(mid + 1, max, target, arr)
+    else:
+        return binarySearch(min, mid - 1, target, arr)
+
+
+N = int(input())
+
+A = [-1_000_000_000]
+
+dp = [-1_000_000_000] * (N+5)
+dpLength = 1
+
+A += list(map(int, input().split(" ")))
+
+for i in range(1, N + 1):
+    idx = binarySearch(0, dpLength - 1, A[i], dp)
+    if idx == -1:
+        dp[dpLength] = A[i]
+        dpLength += 1
+    else:
+        dp[idx] = A[i]
+
+print(dpLength - 1)
