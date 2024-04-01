629b6125d60014b0cb2c63f365bba8387b8ace82
yoon <hunyoon435@gmail.com>
2024-04-01 23:46:26 +0900

    feat: 백준 16435번 문제 해결

---
+++ baekjoon/silver/16435_스네이크버드.py
@@ -0,0 +1,14 @@
+from sys import stdin
+
+input = stdin.readline
+
+N, L = map(int, input().split())
+heights = list(map(int, input().split()))
+
+heights.sort()
+
+for height in heights:
+    if height <= L:
+        L += 1
+
+print(L)
